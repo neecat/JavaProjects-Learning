e08bf0e5972125e4fe0904c51525dd1f1212c62b
neecat <n.karahalis@gmail.com>
2013-07-30 12:17:05 -0400

    Taking a break.

--- HelloWorld/src/library.java
+++ HelloWorld/src/library.java
@@ -1,20 +1,30 @@
 
 public class library {
 	String address;
-	String hours;
 	int numbofbooks=0;
 	Book[] book;
 	void printAvailableBooks()
 	{
-	
+		if(numbofbooks==0)
+		{
+			System.out.println("The library doesn't have any books.");
+		} else{
+			//Look through the library's index
+		}
 	}
 	void borrowBook(String booktitle)
 	{
+		//seek for the book in the library
 	}
 	void addBook(String booktitle)
 	{
 		book[numbofbooks]=new Book(booktitle);
 		numbofbooks++;
 	}
+	void printHours()
+	{
+		System.out.println("The library is open Monday through Friday, 9am to 4pm.");
+	}
+	
 	
 }
